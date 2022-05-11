c54881d0d24effd2d864ea5d59749ad51133c483
ArtHouse17 <102299192+ArtHouse17@users.noreply.github.com>
2022-05-11 14:18:18 +0300

    HW11

---
+++ homework11/hw11.cpp
@@ -0,0 +1,23 @@
+﻿// homework11.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+class Figure {
+
+};
+class Quadrilateral :protected Figure {
+
+};
+class Rhombus : protected Quadrilateral {
+
+};
+class Rectangle : protected Quadrilateral {
+
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
